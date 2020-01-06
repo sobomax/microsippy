9b120bca3e8f63e5bf389f3508fcf7f460aa7c77
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-06 11:39:22 -0800

    Trim \r.

--- scripts/ptyrun.py
+++ scripts/ptyrun.py
@@ -4,5 +4,5 @@ from sys import argv, stdout
 pp = PtyProcessUnicode.spawn(argv[1:])
 while True:
     line = pp.readline()
-    stdout.write(line)
+    stdout.write(line.replace('\r', ''))
     stdout.flush()
