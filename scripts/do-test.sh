26bcc622ce386e062303a3770e460a344e7c250a
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-06 17:39:46 -0800

    Save ourbound MSG into temp file and compare it to the message that comes back.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -9,7 +9,8 @@ TOOLSDIR=`dirname ${TOOLSPATH}`
 . "${TOOLSDIR}/test.common.sub"
 
 MLOG="${BUILDDIR}/monitor.log"
-RESLOG="${BUILDDIR}/100trying.res"
+REQFILE="${BUILDDIR}/100trying.req"
+RESFILE="${BUILDDIR}/100trying.res"
 
 cd "${SRCDIR}"
 PATH="${PATH}:${IDF_TOOLCHAIN}/bin" python "${TOOLSDIR}/ptyrun.py" -o "${MLOG}" \
@@ -20,8 +21,8 @@ sleep 5
 BRD_IP=`grep 'sta ip: ' "${MLOG}" | sed 's|.*sta ip: ||; s|,.*||'`
 if [ ! -z "${BRD_IP}" ]
 then
-  sed "s|%%BRD_IP%%|${BRD_IP}|g" ${TOOLSDIR}/100trying.raw | \
-   nc -w 1 -u "${BRD_IP}" 5060 > "${RESLOG}"
+  sed "s|%%BRD_IP%%|${BRD_IP}|g" "${TOOLSDIR}/100trying.raw" > "${REQFILE}"
+  nc -w 1 -u "${BRD_IP}" 5060 < "${REQFILE}" > "${RESFILE}"
   sleep 15
 fi
 kill -TERM ${MON_PID}
@@ -32,3 +33,4 @@ then
 fi
 grep -q 'Waiting for data' "${MLOG}"
 grep -q 'SIP/2.0 100 Trying' "${MLOG}"
+${DIFF} "${REQFILE}" "${RESFILE}"
