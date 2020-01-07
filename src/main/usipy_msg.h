d163eb573dc0e57b2dd58dbd3c52ec15b865f0de
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-06 16:52:21 -0800

    Add missing "struct". C++ etches your brain. :)

--- src/main/usipy_msg.h
+++ src/main/usipy_msg.h
@@ -2,8 +2,8 @@ enum usipy_msg_kind {SIP_REQ, SIP_RES};
 
 struct usipy_msg {
    enum usipy_msg_kind kind;
-   usipy_str onwire;
+   struct usipy_str onwire;
 };
 
-struct usipy_msg *usipy_msg_ctor_fromwire(usipy_str_ro *wirebuf);
+struct usipy_msg *usipy_msg_ctor_fromwire(struct usipy_str_ro *wirebuf);
 void usipy_msg_dtor(struct usipy_msg *);
