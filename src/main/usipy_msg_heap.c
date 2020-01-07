926e1f94adc714e3107f170837548390cd0b44ea
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-07 12:42:10 -0800

    Make sure we keep allocating chunks at proper boundary.

--- src/main/usipy_msg_heap.c
+++ src/main/usipy_msg_heap.c
@@ -6,9 +6,13 @@
 void *
 usipy_msg_heap_alloc(struct usipy_msg_heap *hp, size_t len)
 {
-    size_t currfree;
+    size_t currfree, alen;
     void *rp;
 
+    alen = len & ~((1 << USIPY_MEM_ALIGNOF) - 1);
+    if (alen != len) {
+        len = alen + (1 << USIPY_MEM_ALIGNOF);
+    }
     currfree = hp->size - (hp->free - hp->first);
     if (currfree < len)
         return (NULL);
