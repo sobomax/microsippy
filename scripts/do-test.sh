33ebbe49dc6c24613b28c1b71d68e4963cf3221c
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-06 17:15:22 -0800

    o Move monitor.log into BUILDDIR;
    o save incoming packets into ${BUILDDIR}/100trying.res.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -8,16 +8,20 @@ TOOLSDIR=`dirname ${TOOLSPATH}`
 
 . "${TOOLSDIR}/test.common.sub"
 
+MLOG="${BUILDDIR}/monitor.log"
+RESLOG="${BUILDDIR}/100trying.res"
+
 cd "${SRCDIR}"
-PATH="${PATH}:${IDF_TOOLCHAIN}/bin" python "${TOOLSDIR}/ptyrun.py" -o monitor.log \
+PATH="${PATH}:${IDF_TOOLCHAIN}/bin" python "${TOOLSDIR}/ptyrun.py" -o "${MLOG}" \
   ${IDF_PATH}/tools/idf.py monitor &
 MON_RC=${?}
 MON_PID=${!}
 sleep 5
-BRD_IP=`grep 'sta ip: ' monitor.log | sed 's|.*sta ip: ||; s|,.*||'`
+BRD_IP=`grep 'sta ip: ' "${MLOG}" | sed 's|.*sta ip: ||; s|,.*||'`
 if [ ! -z "${BRD_IP}" ]
 then
-  sed "s|%%BRD_IP%%|${BRD_IP}|g" ${TOOLSDIR}/100trying.raw | nc -w 1 -u "${BRD_IP}" 5060
+  sed "s|%%BRD_IP%%|${BRD_IP}|g" ${TOOLSDIR}/100trying.raw | \
+   nc -w 1 -u "${BRD_IP}" 5060 > "${RESLOG}"
   sleep 15
 fi
 kill -TERM ${MON_PID}
@@ -26,5 +30,5 @@ if [ -z "${BRD_IP}" ]
 then
   exit 1
 fi
-grep -q 'Waiting for data' monitor.log
-grep -q 'SIP/2.0 100 Trying' monitor.log
+grep -q 'Waiting for data' "${MLOG}"
+grep -q 'SIP/2.0 100 Trying' "${MLOG}"
