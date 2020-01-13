540918ad40ab44cf67338a48b141135b13fc25a4
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-13 14:40:04 -0800

    Add missing semicoln.

--- src/main/usipy_sip_hdr.h
+++ src/main/usipy_sip_hdr.h
@@ -2,6 +2,6 @@ struct usipy_sip_hdr {
     struct {
         struct usipy_str full;
         struct usipy_str name;
-        struct usipy_str value
+        struct usipy_str value;
     } onwire;
 };
