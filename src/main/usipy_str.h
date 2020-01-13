b5e3ede9521f1b5d05ba2b71dd67cbba0ae301c9
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-13 15:25:05 -0800

    Make new macros actually working.

--- src/main/usipy_str.h
+++ src/main/usipy_str.h
@@ -16,8 +16,8 @@ struct usipy_str_ro {
 int usipy_str_split(const struct usipy_str *, unsigned char,
   struct usipy_str *, struct usipy_str *);
 
-#define usipy_str_trm_e(sp, USIPY_ISWS) \
-    while ((sp)->l > 0 && _fc((sp)->s.ro[(sp)->l - 1])) { \
+#define usipy_str_trm_e(sp) \
+    while ((sp)->l > 0 && USIPY_ISWS((sp)->s.ro[(sp)->l - 1])) { \
         (sp)->l -= 1; \
     }
 
@@ -27,8 +27,8 @@ int usipy_str_split(const struct usipy_str *, unsigned char,
         (sp)->l -= 1; \
     }
 
-#define usipy_str_ltrm_e(sp, USIPY_ISLWS) \
-    while ((sp)->l > 0 && _fc((sp)->s.ro[(sp)->l - 1])) { \
+#define usipy_str_ltrm_e(sp) \
+    while ((sp)->l > 0 && USIPY_ISLWS((sp)->s.ro[(sp)->l - 1])) { \
         (sp)->l -= 1; \
     }
 
