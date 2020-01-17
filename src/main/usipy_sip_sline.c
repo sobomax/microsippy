8339d4ab5a4f89cecfe9b0ab438159798c558fa4
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-16 16:51:24 -0800

    Make compiling.

--- src/main/usipy_sip_sline.c
+++ src/main/usipy_sip_sline.c
@@ -8,7 +8,7 @@ usipy_sip_sline_parse(struct usipy_sip_sline *slp)
 {
     struct usipy_str s1, s2, s3, s4;
 
-    if (usipy_str_split(sp->onwire, USIPY_SP, &s1, &s2) != 0)
+    if (usipy_str_split(&slp->onwire, USIPY_SP, &s1, &s2) != 0)
         return (-1);
     slp->parsed.sl.version = s1;
     if (usipy_str_split(&s2, USIPY_SP, &s3, &s4) != 0)
