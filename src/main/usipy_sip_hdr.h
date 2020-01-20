658ec55dee74e1051fc195700033a86d2fc76265
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-20 11:21:47 -0800

    Add pointer to a parsed header structure.

--- src/main/usipy_sip_hdr.h
+++ src/main/usipy_sip_hdr.h
@@ -1,4 +1,9 @@
 struct usipy_hdr_db_entr;
+struct usipy_sip_hdr_cseq;
+
+union usipy_sip_hdr_parsed {
+    struct usipy_sip_hdr_cseq *cseq;
+};
 
 struct usipy_sip_hdr {
     struct {
@@ -8,6 +13,7 @@ struct usipy_sip_hdr {
 	const struct usipy_hdr_db_entr *hf_type;
     } onwire;
     const struct usipy_hdr_db_entr *hf_type;
+    union usipy_sip_hdr_parsed parsed;
 };
 
 int usipy_sip_hdr_preparse(struct usipy_sip_hdr *);
