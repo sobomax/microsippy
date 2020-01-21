64bbb343459d55c8d62d50df1434cb2ec2bcc2db
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-21 13:09:31 -0800

    Revert "Revert back to using xthal_get_ccount now that we know how to"
    This reverts commit 975703759adb385d7238f9bd573fa2e6e283c931.

--- src/main/usipy_sip_tm.c
+++ src/main/usipy_sip_tm.c
@@ -6,7 +6,6 @@
 #include "lwip/sockets.h"
 #include "lwip/inet.h"
 #include "esp_log.h"
-#include "xtensa/hal.h"
 
 #include "usipy_str.h"
 #include "usipy_sip_tm.h"
@@ -20,17 +19,53 @@
 #include "usipy_sip_hdr_types.h"
 #include "usipy_sip_hdr_db.h"
 
-#define TS_CCOUNT_MAX 0x7FFFFF
+#define ESP8266_REG(addr) *((volatile uint32_t *)(0x60000000+(addr)))
+#define T1I  ESP8266_REG(0x60C) //Interrupt Status Register (1bit) write to clear
+#define T1V  ESP8266_REG(0x604) //(RO) Current Value
+#define T1C  ESP8266_REG(0x608) //Control Register
+#define T1L  ESP8266_REG(0x600) //Load Value (Starting Value of Counter) 23bit (0-8388607)
+#define TCIT  0 //Interrupt Type 0:edge, 1:level
+#define TCPD  2 //Prescale Devider (2bit) 0:1(12.5ns/tick), 1:16(0.2us/tick), 2/3:256(3.2us/tick)
+#define TCAR  6 //AutoReload (restart timer when condition is reached)
+#define TCTE  7 //Timer Enable
+#define T1VMAX 0x7FFFFF
+#define timer1_read()           (T1V)
+#define timer1_enabled()        ((T1C & (1 << TCTE)) != 0)
+
+//timer dividers
+enum TIM_DIV_ENUM {
+  TIM_DIV1 = 0,   //80MHz (80 ticks/us - 104857.588 us max)
+  TIM_DIV16 = 1,  //5MHz (5 ticks/us - 1677721.4 us max)
+  TIM_DIV256 = 3 //312.5Khz (1 tick = 3.2us - 26843542.4 us max)
+};
+//timer reload values
+#define TIM_SINGLE      0 //on interrupt routine you need to write a new value to start the timer again
+#define TIM_LOOP        1 //on interrupt the counter will start with the same value again
+#define TIM_EDGE        0
+
+static void
+timer1_enable(uint8_t divider, uint8_t int_type, uint8_t reload){
+    T1C = (1 << TCTE) | ((divider & 3) << TCPD) | ((int_type & 1) << TCIT) | ((reload & 1) << TCAR);
+    T1I = 0;
+}
+
+static void
+timer1_write(uint32_t ticks){
+    T1L = ((ticks)& T1VMAX);
+#if 0
+    if ((T1C & (1 << TCIT)) == 0) TEIE |= TEIE1;//edge int enable
+#endif
+}
 
 static unsigned int
 tsdiff(unsigned int bts, unsigned int ets)
 {
     unsigned int r;
 
-    if (bts <= ets)
-        return (ets - bts);
-    r = (unsigned int)TS_CCOUNT_MAX - bts;
-    return (r + ets + 1);
+    if (ets <= bts)
+        return (bts - ets);
+    r = (unsigned int)T1VMAX - ets;
+    return (r + bts + 1);
 }
 
 void
@@ -43,6 +78,8 @@ usipy_sip_tm_task(void *pvParameters)
     const struct usipy_sip_tm_conf *cfp;
 
     cfp = (struct usipy_sip_tm_conf *)pvParameters;
+    timer1_enable(TIM_DIV1, TIM_EDGE, TIM_LOOP);
+    timer1_write(0xffffffff);
     while (1) {
         union {
             struct sockaddr_in v4;
@@ -138,17 +175,19 @@ usipy_sip_tm_task(void *pvParameters)
 
                 int cerror;
                 unsigned int bts, ets;
-                bts = xthal_get_ccount();
+                bts = timer1_read();
                 struct usipy_msg *msg = usipy_sip_msg_ctor_fromwire(rx_buffer, len, &cerror);
-                ets = xthal_get_ccount();
+                ets = timer1_read();
                 ESP_LOGI(cfp->log_tag, "usipy_sip_msg_ctor_fromwire() = %p: took tsdiff(%u, %u) = %u cycles",
                   msg, bts, ets, tsdiff(bts, ets));
+                ESP_LOGI(cfp->log_tag, "timer1_enabled() = %u, timer1_read() = %u:%u",
+                  timer1_enabled(), timer1_read(), timer1_read());
                 if (msg == NULL)
                     continue;
 #define USIPY_HF_TID_MASK (USIPY_HFT_MASK(USIPY_HF_CSEQ) | USIPY_HFT_MASK(USIPY_HF_CALLID))
-                bts = xthal_get_ccount();
+                bts = timer1_read();
                 int rval = usipy_sip_msg_parse_hdrs(msg, USIPY_HF_TID_MASK);
-                ets = xthal_get_ccount();
+                ets = timer1_read();
                 ESP_LOGI(cfp->log_tag, "usipy_sip_msg_parse_hdrs(USIPY_HF_TID_MASK) = %d: took %u cycles", rval,
                   tsdiff(bts, ets));
                 usipy_sip_msg_dump(msg, cfp->log_tag);
