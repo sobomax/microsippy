73b8ad6835cd7b044d2787188e5dca8344cb982c
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-27 09:21:26 -0800

    GC debug code.

--- src/main/usipy_sip_msg.c
+++ src/main/usipy_sip_msg.c
@@ -70,10 +70,6 @@ usipy_sip_msg_ctor_fromwire(const char *buf, size_t len,
         if (crlf_off < 0)
             break;
         const char *chp = rp->onwire.s.ro + crlf_off;
-#if 0
-        ESP_LOGI("foobar", "rp->nhdrs = %d, chp = %p, cp.s.ro == %p",
-          rp->nhdrs, chp, cp.s.ro);
-#endif
         if (rp->nhdrs > 0) {
             if (chp == cp.s.ro) {
                 /* End of headers reached */
@@ -92,10 +88,6 @@ usipy_sip_msg_ctor_fromwire(const char *buf, size_t len,
             rp->sline.onwire.s.ro = cp.s.ro;
             rp->sline.onwire.l = crlf_off;
             rp->kind = usipy_sip_sline_parse(&rp->sline);
-#if 0
-            ESP_LOGI("foobar", "usipy_sip_sline_parse(\"%.*s\") = %d",
-              rp->sline.onwire.l, rp->sline.onwire.s.ro, rp->kind);
-#endif
             if (rp->kind == USIPY_SIP_MSG_UNKN)
                 goto e1;
             goto next_line;
@@ -115,20 +107,9 @@ next_line:
     if (rp->nhdrs == 0) {
         goto e1;
     }
-#if 0
-    ESP_LOGI("foobar", "rp->nhdrs = %d", rp->nhdrs);
-#endif
     for (int i = 0; i < rp->nhdrs; i++) {
 	struct usipy_sip_hdr *tsp = &(rp->hdrs[i]);
-#if 0
-        ESP_LOGI("foobar", "usipy_sip_sline_parse(\"%.*s\")",
-          tsp->onwire.full.l, tsp->onwire.full.s.ro);
-#endif
         if (usipy_sip_hdr_preparse(tsp) != 0) {
-#if 0
-            ESP_LOGI("foobar", "usipy_sip_hdr_preparse failed at %d", i);
-            goto e1;
-#endif
         }
         rp->hdr_masks.present |= USIPY_HF_MASK(tsp);
     }
