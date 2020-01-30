371cc10d5d22994cad1d7b63f95ce8a3a38ba256
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-29 16:52:54 -0800

    Grep results of the "usipy_sip_msg_parse_hdrs".

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -57,7 +57,7 @@ do
   RESFILE="${BUILDDIR}/${tst}.res"
   ${DIFF} "${EXPFILE}" "${RESFILE}"
 done
-for s in "usipy_sip_msg_ctor_fromwire" "heap remaining"
+for s in "usipy_sip_msg_ctor_fromwire" "usipy_sip_msg_parse_hdrs" "heap remaining"
 do
   grep -w "${s}" "${MLOG}"
 done
