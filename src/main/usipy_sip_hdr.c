99c858a4b01202fa57b7fa5f46bb3d5b29e9c15f
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-30 14:33:42 -0800

    Make compiling.

--- src/main/usipy_sip_hdr.c
+++ src/main/usipy_sip_hdr.c
@@ -6,42 +6,39 @@
 #include "usipy_sip_hdr_db.h"
 
 int
-usipy_sip_hdr_preparse(struct usipy_sip_hdr **shp)
+usipy_sip_hdr_preparse(struct usipy_sip_hdr *shp)
 {
      const struct usipy_hdr_db_entr *hdep;
-     struct usipy_sip_hdr *chp;
      int nextra;
 
-     chp = *shp;
-     if (usipy_str_split(&chp->onwire.full, ':', &chp->onwire.name,
-       &chp->onwire.value) != 0)
+     if (usipy_str_split(&shp->onwire.full, ':', &shp->onwire.name,
+       &shp->onwire.value) != 0)
          return (-1);
-     usipy_str_trm_e(&chp->onwire.name);
-     hdep = usipy_hdr_db_lookup(&chp->onwire.name);
+     usipy_str_trm_e(&shp->onwire.name);
+     hdep = usipy_hdr_db_lookup(&shp->onwire.name);
      if (hdep == NULL)
 	 return (-1);
-     chp->onwire.hf_type = hdep;
+     shp->onwire.hf_type = hdep;
      if (!usipy_hdr_iscmpct(hdep)) {
-         chp->hf_type = hdep;
+         shp->hf_type = hdep;
      } else {
-         chp->hf_type = usipy_hdr_db_byid(hdep->cantype);
+         shp->hf_type = usipy_hdr_db_byid(hdep->cantype);
      }
      nextra = 0;
      if (hdep->flags.csl_allowed) {
-         for (struct usipy_str csp = chp->onwire.value; csp.l > 0;) {
-             if (usipy_str_split(&csp, ',', &chp->onwire.value, &csp) != 0)
+         for (struct usipy_str csp = shp->onwire.value; csp.l > 0;) {
+             if (usipy_str_split(&csp, ',', &shp->onwire.value, &csp) != 0) {
                  break;
              }
-             usipy_str_ltrm_b(&chp->onwire.value);
-             usipy_str_ltrm_e(&chp->onwire.value);
+             usipy_str_ltrm_b(&shp->onwire.value);
+             usipy_str_ltrm_e(&shp->onwire.value);
              nextra += 1;
-             chp++;
-             chp->hf_type = chp[-1].hf_type;
-             chp->onwire.name = chp[-1].onwire.name;
-             (*shp)++;
+             shp++;
+             shp->hf_type = shp[-1].hf_type;
+             shp->onwire.name = shp[-1].onwire.name;
          }
      }
-     usipy_str_ltrm_b(&chp->onwire.value);
-     usipy_str_ltrm_e(&chp->onwire.value);
+     usipy_str_ltrm_b(&shp->onwire.value);
+     usipy_str_ltrm_e(&shp->onwire.value);
      return (nextra);
 }
