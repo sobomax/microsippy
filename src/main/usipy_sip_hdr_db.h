519b37ca761906d38bf19e1f6bdfc228a0d73db4
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-30 13:58:43 -0800

    Add a flag to indicate when multiple header values can be packed into one HF entry for a specific HF type.

--- src/main/usipy_sip_hdr_db.h
+++ src/main/usipy_sip_hdr_db.h
@@ -1,6 +1,11 @@
 struct usipy_hdr_db_entr {
     struct usipy_str name;
-    unsigned char cantype;
+    unsigned char cantype:7;
+    /*
+     * Set if multiple header fields of the same field name can be combined
+     * into one comma-separated list for this type of header field.
+     */
+    unsigned char csl_allowed:1;
 };
 
 const struct usipy_hdr_db_entr *usipy_hdr_db_lookup(const struct usipy_str *);
