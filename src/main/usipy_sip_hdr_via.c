6f087373b7d537c1f21199fbef7ff4074bcfc934
Maksym Sobolyev <sobomax@sippysoft.com>
2020-01-31 16:00:28 -0800

    Preallocate space and fill that space as we go.

--- src/main/usipy_sip_hdr_via.c
+++ src/main/usipy_sip_hdr_via.c
@@ -13,35 +13,41 @@ struct usipy_sip_hdr_via *
 usipy_sip_hdr_via_parse(struct usipy_msg_heap *mhp,
   const struct usipy_str *hvp)
 {
-    struct usipy_str s1, s2, s3, s4;
-    struct usipy_str sent_by;
+    struct usipy_str s4;
+    struct usipy_str sent_by, sent_by_port;
     struct usipy_sip_hdr_via *vp;
-
-    if (usipy_str_split3(hvp, '/', &s1, &s2, &s4) != 0) {
-        return (NULL);
-    }
-    usipy_str_ltrm_b(&s4); /* UDP */
-    if (usipy_str_splitlws(&s4, &s3, &s4) != 0) {
-        return (NULL);
-    }
-    usipy_str_ltrm_e(&s1); /* SIP */
-    usipy_str_ltrm_b(&s2); /* 2.0 */
-    usipy_str_ltrm_e(&s2);
-    usipy_str_ltrm_b(&s4);
-    if (usipy_str_split(&s4, ';', &sent_by, &s4) != 0) {
-        sent_by = s4;
-    } else {
-        usipy_str_ltrm_e(&sent_by);
-    }
 
     vp = usipy_msg_heap_alloc(mhp, sizeof(struct usipy_sip_hdr_via));
     if (vp == NULL) {
         return (NULL);
     }
-    vp->sent_by.host = sent_by;
-    vp->sent_protocol.name = s1;
-    vp->sent_protocol.version = s2;
-    vp->sent_protocol.transport = s3;
+    if (usipy_str_split3(hvp, '/', &vp->sent_protocol.name, &vp->sent_protocol.version, &s4) != 0) {
+        return (NULL);
+    }
+    usipy_str_ltrm_b(&s4); /* UDP */
+    if (usipy_str_splitlws(&s4, &vp->sent_protocol.transport, &s4) != 0) {
+        return (NULL);
+    }
+    usipy_str_ltrm_e(&vp->sent_protocol.name); /* SIP */
+    usipy_str_ltrm_b(&vp->sent_protocol.version); /* 2.0 */
+    usipy_str_ltrm_e(&vp->sent_protocol.version);
+    usipy_str_ltrm_b(&s4);
+    if (usipy_str_split(&s4, ';', &sent_by, &s4) != 0) {
+        sent_by = s4;
+        s4.l = 0;
+    } else {
+        usipy_str_ltrm_e(&sent_by);
+    }
+    if (sent_by.l == 0) {
+        return (NULL);
+    }
+    if (usipy_str_split(&sent_by, ':', &vp->sent_by.host, &sent_by_port) == 0) {
+        usipy_str_ltrm_e(&vp->sent_by.host);
+        usipy_str_ltrm_b(&sent_by_port);
+    } else {
+        vp->sent_by.host = sent_by;
+    }
+
     return (vp);
 }
 
