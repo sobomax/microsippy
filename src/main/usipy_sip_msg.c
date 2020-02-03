bba44d3e181daaf2feb913bec4374b1323fbd78d
Maksym Sobolyev <sobomax@sippysoft.com>
2020-02-03 13:39:21 -0800

    Include "usipy_tvpair.h".

--- src/main/usipy_sip_msg.c
+++ src/main/usipy_sip_msg.c
@@ -16,6 +16,7 @@
 #include "usipy_sip_hdr_db.h"
 #include "usipy_sip_method_db.h"
 #include "usipy_sip_hdr_cseq.h"
+#include "usipy_tvpair.h"
 #include "usipy_sip_hdr_via.h"
 #include "usipy_sip_uri.h"
 
