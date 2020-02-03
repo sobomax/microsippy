151cf42f5cb30d321fbf273bf06684dcc04b2816
Maksym Sobolyev <sobomax@sippysoft.com>
2020-02-03 13:41:51 -0800

    usipy_sip_param -> usipy_tvpair.

--- src/main/usipy_sip_uri.h
+++ src/main/usipy_sip_uri.h
@@ -7,9 +7,9 @@ struct usipy_sip_uri {
     struct usipy_str host;
     unsigned int port;
     int nparams;
-    struct usipy_sip_param *parameters;
+    struct usipy_tvpair *parameters;
     struct usipy_str headers;
-    struct usipy_sip_param params[0];
+    struct usipy_tvpair params[0];
 };
 
 struct usipy_sip_uri *usipy_sip_uri_parse(struct usipy_msg_heap *,
