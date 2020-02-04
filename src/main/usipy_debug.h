c2dc10a0e12328df418b65a1dc0230f84d59a32f
Maksym Sobolyev <sobomax@sippysoft.com>
2020-02-04 10:57:55 -0800

    Disable debug code for now just to see how much would we gain.

--- src/main/usipy_debug.h
+++ src/main/usipy_debug.h
@@ -1,9 +1,14 @@
 #ifndef _USIPY_DEBUG_H
 #define _USIPY_DEBUG_H
 
-#include <assert.h>
+#if 0
+# include <assert.h>
 
-#define USIPY_DASSERT(x) assert(x)
-#define USIPY_DCODE(code) code
-
+# define USIPY_DASSERT(x) assert(x)
+# define USIPY_DCODE(code) code
+#else
+# define USIPY_DASSERT(x)
+# define USIPY_DCODE(code)
 #endif
+
+#endif /* _USIPY_DEBUG_H */
