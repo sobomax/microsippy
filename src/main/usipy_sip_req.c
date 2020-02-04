aafb624c41f2ef662144f40be22b025dcac87fa1
Maksym Sobolyev <sobomax@sippysoft.com>
2020-02-04 09:49:35 -0800

    Add TO to keep request-related functions in.

---
+++ src/main/usipy_sip_req.c
@@ -0,0 +1,18 @@
+#include "usipy_sip_sline.h"
+#include "usipy_sip_msg.h"
+#include "usipy_sip_req.h"
+
+int
+usipy_sip_req_parse_ruri(struct usipy_msg *mp)
+{
+    assert(mp->kind == USIPY_SIP_MSG_REQ);
+
+    if (mp->sline.parsed.rl.ruri != NULL)
+        return (-1);
+
+    mp->sline.parsed.rl.ruri = usipy_sip_uri_parse(&mp->heap,
+      &mp->sline.parsed.rl.onwire.ruri);
+    if (mp->sline.parsed.rl.ruri == NULL)
+        return (-1);
+    return (0);
+}
