d0877193852c7211cb693ca233c1a812075db1be
Maksym Sobolyev <sobomax@sippysoft.com>
2020-02-05 10:27:39 -0800

    We don't need a prototype for a function that is inline.

--- src/main/usipy_msg_heap_rb.h
+++ src/main/usipy_msg_heap_rb.h
@@ -6,9 +6,4 @@ struct usipy_msg_heap_cnt {
     USIPY_DCODE(size_t lastalen);
 };
 
-void *usipy_msg_heap_alloc_cnt(struct usipy_msg_heap *, size_t,
-  struct usipy_msg_heap_cnt *);
-int usipy_msg_heap_aextend(struct usipy_msg_heap *, size_t,
-  struct usipy_msg_heap_cnt *);
-
 #endif /* _USIPY_MSG_HEAP_RB_H */
