520f58a2f2a9a7367b7849763077b7fca175bcea
Maksym Sobolyev <sobomax@sippysoft.com>
2020-02-05 10:03:15 -0800

    Make second argument const.

--- src/main/usipy_msg_heap_inl.h
+++ src/main/usipy_msg_heap_inl.h
@@ -2,7 +2,7 @@
 #define _USIPY_MSG_HEAP_INL_H
 
 inline void
-usipy_msg_heap_rollback(struct usipy_msg_heap *hp, struct usipy_msg_heap_cnt *cntp)
+usipy_msg_heap_rollback(struct usipy_msg_heap *hp, const struct usipy_msg_heap_cnt *cntp)
 {
 
     USIPY_DASSERT(cntp->lastalen == hp->alen);
