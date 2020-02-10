b3ab79ba3332e465d78d01cb68bfa613eb3b8e75
Maksym Sobolyev <sobomax@sippysoft.com>
2020-02-10 14:35:41 -0800

    Include "usipy_sip_method_types.h".

--- src/main/usipy_sip_tid.c
+++ src/main/usipy_sip_tid.c
@@ -7,6 +7,7 @@
 #include "usipy_sip_hdr.h"
 #include "usipy_sip_hdr_cseq.h"
 #include "usipy_sip_method_db.h"
+#include "usipy_sip_method_types.h"
 #include "usipy_sip_tid.h"
 
 static inline uint32_t
