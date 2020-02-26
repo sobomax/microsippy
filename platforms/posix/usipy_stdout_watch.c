c9124d1297fe21c822696c97a87914d94e1b68b1
Maksym Sobolyev <sobomax@sippysoft.com>
2020-02-25 22:53:06 -0800

    INFTIM is not presend on linex.

--- platforms/posix/usipy_stdout_watch.c
+++ platforms/posix/usipy_stdout_watch.c
@@ -5,6 +5,12 @@
 
 #include "usipy_stdout_watch.h"
 
+#ifdef INFTIM
+#define POLL_INFTIM INFTIM
+#else
+#define POLL_INFTIM (-1)
+#endif
+
 void *
 usipy_stdout_watch(void *tap)
 {
@@ -14,7 +20,7 @@ usipy_stdout_watch(void *tap)
     pfds[0].fd = fileno(stdout);
     pfds[0].events = POLLHUP | POLLIN;
     do {
-        r = poll(pfds, 1, INFTIM);
+        r = poll(pfds, 1, POLL_INFTIM);
     } while (r == 0 || (r < 0 && errno == EINTR));
     exit(0);
 }
