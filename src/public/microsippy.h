46224de6b0130fe37752f6a0c98149c04a29f221
Maksym Sobolyev <sobomax@sippysoft.com>
2020-11-16 18:10:34 -0800

    Add root header to unite them all.

---
+++ src/public/microsippy.h
@@ -0,0 +1,11 @@
+#ifndef _microsippy_h
+#define _microsippy_h
+
+#include "usipy_str.h"
+#include "usipy_msg_heap.h"
+#include "usipy_sip_sline.h"
+#include "usipy_sip_hdr_types.h"
+#include "usipy_sip_method_types.h"
+#include "usipy_sip_msg.h"
+
+#endif
